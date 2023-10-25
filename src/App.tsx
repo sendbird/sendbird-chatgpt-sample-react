8898dc130a3cd5d2ea088878dfb7a909c1b1d1b9
Irene Ryu <irene.ryu@sendbird.com>
2023-10-25 17:07:07 +0900

    change replyType default -> thread

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ export default function Chat() {
       userId={userId}
       nickname={nickname}
       isVoiceMessageEnabled={false}
+      replyType="THREAD"
     >
       <div id='sendbird-chatgpt-demo'>
         <div id='sendbird-chatgpt-demo__channel-list'>
