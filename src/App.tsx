46fc7869b607b5789a8c085ac6ad0c0e12ba07f7
Irene Ryu <irene.ryu@sendbird.com>
2023-10-27 10:54:32 +0900

    Disable custom features

--- src/App.tsx
+++ src/App.tsx
@@ -3,54 +3,20 @@ import '@sendbird/uikit-react/dist/index.css'
 import './App.css'
 import './disabled-features.css'
 
-import { useState } from 'react'
-import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider'
-import ChannelList from '@sendbird/uikit-react/ChannelList'
-import Channel from '@sendbird/uikit-react/Channel'
-
+import App from '@sendbird/uikit-react/App'
 import { appId, nickname, userId } from './consts'
-import { ChannelSettings } from '@sendbird/uikit-react'
-import ChannelListHeader from './Components/ChannelListHeader'
-import TypingIndicator from './Components/TypingIndicator'
 
 export default function Chat() {
-  const [currentChannelUrl, setCurrentChannelUrl] = useState<string>('')
-  const [showSettings, setShowSettings] = useState<boolean>(false)
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   return (
-    <SendbirdProvider
+    <App
       appId={appId}
       userId={userId}
       nickname={nickname}
       isVoiceMessageEnabled={false}
+      breakpoint={isMobile}
+      showSearchIcon={true}
       replyType="THREAD"
-    >
-      <div id='sendbird-chatgpt-demo'>
-        <div id='sendbird-chatgpt-demo__channel-list'>
-          <ChannelList
-            onChannelSelect={(channel) => {
-              setCurrentChannelUrl(channel?.url)
-            }}
-            renderHeader={ChannelListHeader}
-          />
-        </div>
-        <div id='sendbird-chatgpt-demo__channel'>
-          <Channel
-            channelUrl={currentChannelUrl}
-            onChatHeaderActionClick={() => setShowSettings(!showSettings)}
-            renderTypingIndicator={TypingIndicator}
-          />
-        </div>
-        {
-          showSettings && (
-            <div id='sendbird-chatgpt-demo__settings'>
-              <ChannelSettings
-                onCloseClick={() => setShowSettings(false)}
-                channelUrl={currentChannelUrl}
-              />
-            </div>
-          )
-        }
-      </div>
-    </SendbirdProvider>
+    />
   )
 }
